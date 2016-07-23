f11e39b05470362c98305ea9d223086954560952
Garcia Basile <basilegarcia@gmail.com>
2016-07-23 14:51:20 +0200

    Create c_shell.c
    Used in order to make people believe your a c programmer, eventually to short some commands.

---
+++ c_shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.io>
+
+int main()
+{   popen("python yourscript.py", "r");
+    return 0;
+}
+
+/* This scripts is intented to be used by noobs and c programmer wannabes.
+ * Once you became one you have to compile it and put the executable file  in
+ * /usr/bin/ directory to run it directly from your favorite terminal.
+ */ You may now be able to show off your c skills.
