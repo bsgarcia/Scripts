eaa50e12492d5707b77561bc104e1d2af7d34944
Garcia Basile <basilegarcia@gmail.com>
2016-07-23 19:08:16 +0200

    Update c_shell.c

--- c_shell.c
+++ c_shell.c
@@ -7,7 +7,7 @@ int main()
 }
 
 /* This scripts is intented to be used by noobs and c programmer wannabes.
- * Once you became one you have to compile it and put the executable file  in
+ * Once you became one you have to compile this and put the executable file  in
  * /usr/bin/ directory to run it directly from your favorite terminal.
  * You may now be able to show off your c skills.
  */
