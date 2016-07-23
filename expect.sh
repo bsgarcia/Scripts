d34ad1356550fd28730fc23823967a8b69f8a60d
Garcia Basile <basilegarcia@gmail.com>
2016-07-23 14:36:23 +0200

    Comments added to expect.sh

--- expect.sh
+++ expect.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/expect
+
+# Can be used to launch some shell commmands automatically... 
+# "expect" commands may be associated with bash like :
+
+echo start
+spawn irssi
+expect "connection"
+send "/nick random\r"
+interact 
+echo done
+
+# However, I noticed that using expect alone makes the execution WAY quicker than using it with bash. 
+# Indeed in the first case the delay between each request is 
+# by far shorter.
+
+#                     ///THE REAL CODE STARTS BELOW /// 
+
+# Spawn a shell and run an initial command (here we launch an ssh session)
 eval spawn ssh -oStrictHostKeyChecking=no -oCheckHostIP=no test@test.fr 
-#use correct prompt
-set prompt ":"
+
+# Use correct prompt
+set prompt "[:/$ / etc...]"
+
+# Interact with the program launched above 
+
 interact -o -nobuffer -re $prompt return
 send "yourpassword\r"
 interact -o -nobuffer -re $prompt return
