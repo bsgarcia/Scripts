35d45bd4055016ae2ebac3cd94822e667f98590c
Garcia Basile <basilegarcia@gmail.com>
2017-01-15 23:20:58 +0100

    Update c_shell.c

--- c_shell.c
+++ c_shell.c
@@ -6,8 +6,8 @@ int main()
     return 0;
 }
 
-/* This scripts is intented to be used by noobs and c programmer wannabes.
+/* This scripts is intented to be used by noobs and C programmer wannabes.
  * Once you became one you have to compile this and put the executable file  in
  * /usr/bin/ directory to run it directly from your favorite terminal.
- * You may now be able to show off your c skills.
+ * You may now be able to show off your C skills.
  */
