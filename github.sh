588276e0b1bcdb76e2496d1cf7bc5648056e18a8
Basile Garcia <basilegarcia@gmail.com>
2019-03-15 15:07:09 +0100

    Create github.sh

---
+++ github.sh
@@ -0,0 +1,25 @@
+set -e 
+
+echo 'github username:'
+read username
+echo 'Creating repo based on current directory name...'
+
+dir=$(basename $(pwd))
+str='{"name":"'"$dir"'", "description":"Research project"}' 
+msg=$(curl -u $username https://api.github.com/user/repos -d "$str")
+echo '*****************************************************'
+
+case "error" in
+ *"$msg"*) echo 'SOMETHING WENT WRONG'
+esac
+
+echo 'CREATED SUCCESSFULLY'
+echo 'Init and push current repo (caution, all files are added)...'
+
+git init
+git add .
+git commit -a -m "first commit"
+git remote add origin https://github.com/$username/$dir.git
+git push --set-upstream origin master
+
+echo 'DONE'
